c62e28142056c14d8980a39a35e6a00f55e72290
epongpipat <epongpipat@gmail.com>
2023-06-06 16:08:08 -0500

    clean up code and add ensure_permissions at the end

--- bin/qc_copy_report.sh
+++ bin/qc_copy_report.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
 # Copy qc_report to study folder
+
+# modules
 module load bashHelperKennedyRodrigue
+
+# paths
 root_dir=`get_root_dir kenrod`
 code_dir=`dirname $0`
 OUTdir=${root_dir}/study-pams/sourcedata/qc/docs
 
-cp ${code_dir}/qc_report.html $OUTdir/qc_report.html
+# main
+cmd="cp ${code_dir}/qc_report.html ${OUTdir}/qc_report.html"
+echo -e "\ncommand:\n${cmd}\n"
+eval ${cmd}
+
+ensure_permissions ${OUTdir}/qc_report.html
